027e29131c3d2a8ec6696f1bb2ee48aad6f96ad8
Berohlfs <90404673+Berohlfs@users.noreply.github.com>
2024-06-20 14:31:21 -0300

    ADD a form with Form component

--- app/web/app/page.tsx
+++ app/web/app/page.tsx
@@ -1,10 +1,13 @@
 // Components
 import { ToggleTheme } from "./components/ToggleTheme"
+import { LoginForm } from "./components/LoginForm"
 
 export default function Home() {
+
   return (
     <main>
       <ToggleTheme/>
+      <LoginForm/>
     </main>
   )
 }
